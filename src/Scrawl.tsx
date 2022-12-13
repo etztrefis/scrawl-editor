2916906ccd6f42185a4ad02a1bb6bb5a1df61df4
etztrefis <trefis@pm.me>
2022-12-13 23:04:01 +0700

    wrap menu icons with ScrawlTooltipWrapper ⚙️

--- src/Scrawl.tsx
+++ src/Scrawl.tsx
@@ -1,4 +1,5 @@
 import React, { HTMLAttributes } from 'react';
+import ScrawlTooltipWrapper from './Components/Tooltip';
 import { BoldIcon, ItalicIcon, UnderlineIcon } from './icons';
 import './scrawl.default.css';
 
@@ -17,19 +18,47 @@ export const Scrawl = ({
   return (
     <div style={{ width: width, height: height }} className="scrawl-wrapper">
       <div className="scrawl-menu">
-        <button className="menu-button">
-          <BoldIcon className="menu-icon" />
-        </button>
-        <button className="menu-button">
-          <ItalicIcon className="menu-icon" />
-        </button>
-        <button className="menu-button">
-          <UnderlineIcon className="menu-icon" />
-        </button>
+        <ScrawlTooltipWrapper
+          content={
+            <p className="scrawl-tooltip-content">
+              Bold <kbd>Ctrl+B</kbd>
+            </p>
+          }
+          position="top"
+        >
+          <button className="menu-button">
+            <BoldIcon className="menu-icon" />
+          </button>
+        </ScrawlTooltipWrapper>
+        <ScrawlTooltipWrapper
+          content={
+            <p className="scrawl-tooltip-content">
+              Italic <kbd>Ctrl+I</kbd>
+            </p>
+          }
+          position="top"
+        >
+          <button className="menu-button">
+            <ItalicIcon className="menu-icon" />
+          </button>
+        </ScrawlTooltipWrapper>
+        <ScrawlTooltipWrapper
+          content={
+            <p className="scrawl-tooltip-content">
+              Underline <kbd>Ctrl+U</kbd>
+            </p>
+          }
+          position="top"
+        >
+          <button className="menu-button">
+            <UnderlineIcon className="menu-icon" />
+          </button>
+        </ScrawlTooltipWrapper>
       </div>
       <textarea
         {...props}
         className="scrawl"
+        style={{ height: `calc(${height} - 2rem)` }}
         placeholder={placeholder}
       ></textarea>
     </div>
