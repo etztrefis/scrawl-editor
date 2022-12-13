702b9b69716c07882d754d893edd39e72e5d89a0
etztrefis <trefis@pm.me>
2022-12-13 16:21:11 +0700

    css controlled styles 🖌️

--- src/Scrawl.tsx
+++ src/Scrawl.tsx
@@ -1,16 +1,31 @@
 import React, { HTMLAttributes } from 'react';
+import { BoldIcon } from './icons';
+import './scrawl.default.css';
 
 export interface IOScrawlProps extends HTMLAttributes<HTMLTextAreaElement> {
   width?: number | string;
   height?: number | string;
+  placeholder?: string;
 }
 
 export const Scrawl = ({
   width = '100%',
   height = '100%',
+  placeholder = 'Compose an epic...',
   ...props
 }: IOScrawlProps) => {
   return (
-    <textarea {...props} style={{ width: width, height: height }}></textarea>
+    <div style={{ width: width, height: height }} className="scrawl-wrapper">
+      <div className="scrawl-menu">
+        <button className="menu-button">
+          <BoldIcon className="menu-icon" />
+        </button>
+      </div>
+      <textarea
+        {...props}
+        className="scrawl"
+        placeholder={placeholder}
+      ></textarea>
+    </div>
   );
 };
